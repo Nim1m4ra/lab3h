90cbedb8a6980e8d05dfaf644886774bd451648f
)) <Nim949@mail.ru>
2025-10-24 21:00:59 +0300

    калькулятор

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello,world") # ))
+print("Hello, it's simple calculator, print a first number")
+a = int(input())
+print("print second number")
+b = int(input())
+print("What you want to do? (*,//,+,-)")
+c = input()
+if c == "*":
+    print(a * b)
+if c == "//":
+    print(a // b)
+if c == "+":
+    print(a + b)
+if c == "-":
+    print(a - b)
+
+
+
+
+
+
+
+
